d7aac27f59a715401a7d51d1a597df4fca70070f
Kyle Stankovich <gamemakergml@gmail.com>
2019-12-13 11:38:47 -0500

    Added editor utility file to make custom editor layouts easier.

---
+++ Source/stank-utilities/Editor/Utilities/EditorLayoutUtility.cs
@@ -0,0 +1,95 @@
+﻿#if EDITOR
+
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+using StankUtilities.Runtime.Utilities;
+
+namespace StankUtilities.Editor.Utilities
+{
+    /// <summary>
+    /// Useful utility class that makes drawing custom editor GUIs easier.
+    /// </summary>
+    public static class EditorLayoutUtility
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Wrapper for vertical layout.
+        /// </summary>
+        /// <param name="callback">Callback to execute inside layout.</param>
+        /// <param name="style">Style to apply to layout.</param>
+        /// <param name="options">Options for layout.</param>
+        public static void DoVerticalLayout(System.Action callback, GUIStyle style = null, params GUILayoutOption[] options)
+        {
+            if(callback == null)
+            {
+                DebuggerUtility.LogError("Cannot draw vertical layout because provided callback is null!");
+            }
+
+            if(style == null)
+            {
+                EditorGUILayout.BeginVertical(options);
+            }
+            else
+            {
+                EditorGUILayout.BeginVertical(style, options);
+            }
+
+            callback();
+
+            EditorGUILayout.EndVertical();
+        }
+
+        /// <summary>
+        /// Wrapper for horizontal layout.
+        /// </summary>
+        /// <param name="callback">Callback to execute inside layout.</param>
+        /// <param name="style">Style to apply to layout.</param>
+        /// <param name="options">Options for layout.</param>
+        public static void DoHorizontalLayout(System.Action callback, GUIStyle style = null, params GUILayoutOption[] options)
+        {
+            if(callback == null)
+            {
+                DebuggerUtility.LogError("Cannot draw horizontal layout because provided callback is null!");
+            }
+
+            if(style == null)
+            {
+                EditorGUILayout.BeginHorizontal(options);
+            }
+            else
+            {
+                EditorGUILayout.BeginHorizontal(style, options);
+            }
+
+            callback();
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        /// <summary>
+        /// Wrapper for indent level.
+        /// </summary>
+        /// <param name="callback">Callback to execute inside indent.</param>
+        public static void DoIndent(System.Action callback)
+        {
+            if(callback == null)
+            {
+                DebuggerUtility.LogError("Cannot modify indent because provided callback is null!");
+            }
+
+            EditorGUI.indentLevel++;
+
+            callback();
+
+            EditorGUI.indentLevel--;
+        }
+
+#endregion
+    }
+}
+
+#endif
